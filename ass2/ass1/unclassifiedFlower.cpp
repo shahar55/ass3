70b84e5537fd1bc0038761cf325b70a384e2407c
idob10 <barkaido21@gmail.com>
2021-09-10 15:14:20 +0300

    deleted

--- ass2/ass1/unclassifiedFlower.cpp
+++
@@ -1,3 +0,0 @@
-#include "unclassifiedFlower.hpp"
-UnclassifiedFlower::UnclassifiedFlower(double cupLeafWidth,double cupLeafLength,double petalWidth,double petalLength)
-:Flower(cupLeafWidth,cupLeafLength,petalWidth,petalLength){}
